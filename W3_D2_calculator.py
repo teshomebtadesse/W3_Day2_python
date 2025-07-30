8825d604306e589d404de0e763802bd5d8d891d9
teshomebtadesse <beteshe19@gmail.com>
2025-07-30 14:24:53 +0300

    W3_D2_calculator.py

---
+++ W3_D2_calculator.py
@@ -0,0 +1,17 @@
+#Practice creating Python programs to perform arithmetic and string, experimenting with variables, and exploring data types.
+
+#Basic Calculator Program
+
+#Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
+#Perform the operation based on the user's input and print the result.
+#Example: If a user inputs 10, 5, and +, your program should display 10 + 5 = 15.
+
+numb1 = float(input("Enter first number: "))
+numb2 = float(input("Enter second number: "))
+
+print("The sum is: ", numb1 + numb2)
+print("The difference is: ", numb1 - numb2)
+print("The product is: ", numb1 * numb2)
+print("The quotient is: ", numb1 // numb2)
+
+print("That's Great!")
